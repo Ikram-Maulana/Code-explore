c896da88c38715efa3c59ab3f5bd8a916b560272
ikram-maulana <ikram075@ummi.ac.id>
2021-12-21 21:06:35 +0700

    16 - Inheritance

--- TypeScript - NRD/build/class.js
+++ TypeScript - NRD/build/class.js
@@ -5,9 +5,31 @@ exports.Users = void 0;
 class Users {
     constructor(name, age) {
         this.age = age;
+        this.getName = () => {
+            return this.name;
+        };
         this.name = name;
+    }
+    setName(value) {
+        this.name = value;
     }
 }
 exports.Users = Users;
 let users = new Users("Ikram", 21);
 console.log(users);
+// ! Inheritance
+class Admin extends Users {
+    constructor() {
+        super(...arguments);
+        this.read = true;
+        this.write = true;
+    }
+    getRole() {
+        return {
+            read: this.read,
+            write: this.write
+        };
+    }
+}
+let admin = new Admin("Ikram", 21);
+console.log(admin);
--- TypeScript - NRD/src/class.ts
+++ TypeScript - NRD/src/class.ts
@@ -6,6 +6,14 @@ export class Users {
     this.name = name;
   }
 
+  setName(value: string): void {
+    this.name = value;
+  }
+
+  getName = (): string => {
+    return this.name;
+  }
+
   /**
    * yang di atas similar dengan
    * constructor(public name: string) {}
@@ -14,3 +22,20 @@ export class Users {
 
 let users = new Users("Ikram", 21);
 console.log(users);
+
+
+// ! Inheritance
+class Admin extends Users {
+  read: boolean = true;
+  write: boolean = true;
+
+  getRole(): {read: boolean, write: boolean} {
+    return {
+      read: this.read,
+      write: this.write
+    }
+  }
+}
+
+let admin = new Admin("Ikram", 21);
+console.log(admin);
