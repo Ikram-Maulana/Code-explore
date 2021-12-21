be18c44b03b8c2f630298534ba6267bb9ab402bd
ikram-maulana <ikram075@ummi.ac.id>
2021-12-21 10:35:10 +0700

    05 - Tipe Data Array

---
+++ TypeScript - NRD/src/array.ts
@@ -0,0 +1,12 @@
+// ! Array
+let array1: number[];
+array1 = [1, 2, 3];
+console.log(array1);
+
+let array2: string[];
+array2 = ["Iron", "Man"];
+console.log(array2);
+
+let array3: any[];
+array3 = ["ILY", 3000, true];
+console.log(array3);
