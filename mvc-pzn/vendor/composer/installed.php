0d3a63f9c7580e13fd6504dd3525363984bf5a81
ikram-maulana <ikram075@ummi.ac.id>
2022-01-06 18:21:25 +0700

    Membuat Project

---
+++ mvc-pzn/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '139901101f508fea4f1d7fb31ebdf56e1a399180',
+        'name' => 'kelompokmsib/pbo',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kelompokmsib/pbo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '139901101f508fea4f1d7fb31ebdf56e1a399180',
+            'dev_requirement' => false,
+        ),
+    ),
+);
