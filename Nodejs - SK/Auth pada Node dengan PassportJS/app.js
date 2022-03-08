ff8d0c0a35138f2226575de43febb4a94fe6e7b0
ikram-maulana <ikram075@ummi.ac.id>
2022-03-08 09:44:02 +0700

    01 - Intro dan Set Up

---
+++ Nodejs - SK/Auth pada Node dengan PassportJS/app.js
@@ -0,0 +1,46 @@
+// Requirement
+const express = require('express');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Database
+require('./app/utils/db');
+
+// Setup
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(cookieParser('secret'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  cookie: {
+    maxAge: 60000
+  },
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// Setup EJS
+app.set('view engine', 'ejs');
+
+// Route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Run App
+app.listen(port, () => {
+  console.log('Server is running on port ' + port);
+});
---
+++ Nodejs - SK/Auth pada Node dengan PassportJS/note.md
@@ -0,0 +1,7 @@
+# Catatan Pembelajaran
+
+## 01 - Intro dan Set Up  
+- **initialize** folder
+- Install `passport passport-local express express-session cookie-parser body-parser connect-flash bcrypt nodemon mongoose ejs`
+- Buat folder `views, app > models & utils, public`
+- Ke `app.js` lakukan `require`, `database connect` dari **utils**, `Setup Middleware`, `ViewEngine`, `Routing` dan `Run APP`
