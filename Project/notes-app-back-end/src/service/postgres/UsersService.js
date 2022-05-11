5a0192f0e23b9ef0517d039c12a04a1dbe30eb25
ikram-maulana <ikram075@ummi.ac.id>
2022-05-11 10:28:00 +0700

    1.22. Membuat UsersService - Membuat Fungsi addUser dan verifyNewUsername

--- Project/notes-app-back-end/src/service/postgres/UsersService.js
+++ Project/notes-app-back-end/src/service/postgres/UsersService.js
@@ -1,10 +1,54 @@
 const {
   Pool,
 } = require('pg');
+const nanoid = require('nanoid');
+const bcrypt = require('bcrypt');
+const InvariantError = require('../../exceptions/InvariantError');
 
 class UsersService {
   constructor() {
     this._pool = new Pool();
+  }
+
+  async addUser({
+    username,
+    password,
+    fullname,
+  }) {
+    // TODO: Verifikasi username, pastikan belum terdaftar.
+    // TODO: Bila verifikasi lolos, maka masukkan user baru ke database.
+    await this.verifyNewUsername(username);
+
+    const id = `user-${nanoid(16)}`;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const query = {
+      text: 'INSERT INTO users (id, username, password, fullname) VALUES ($1, $2, $3, $4) RETURNING id',
+      values: [id, username, hashedPassword, fullname],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (result.rows.length === 0) {
+      throw new InvariantError('User gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async verifyNewUsername(username) {
+    const query = {
+      text: 'SELECT * FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (result.rows.length > 0) {
+      throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
+    }
+
+    return result.rows.length === 0;
   }
 }
 
