ff442fae4211933d7aac808208c279e5e969121e
ikram-maulana <ikram075@ummi.ac.id>
2022-05-16 19:43:34 +0700

    1.52. Membangun Fitur Kolaborasi Catatan - Membuat Fungsi verifyNoteAccess di NotesService

--- Project/notes-app-back-end/src/service/postgres/NotesService.js
+++ Project/notes-app-back-end/src/service/postgres/NotesService.js
@@ -12,8 +12,9 @@ const {
 } = require('../../utils');
 
 class NotesService {
-  constructor() {
+  constructor(collaborationService) {
     this._pool = new Pool();
+    this._collaborationService = collaborationService;
   }
 
   async addNote({
@@ -111,6 +112,22 @@ class NotesService {
 
     if (note.owner !== owner) {
       throw new AuthorizationError('Anda tidak berhak mengakses resource ini');
+    }
+  }
+
+  async verifyNoteAccess(noteId, userId) {
+    try {
+      await this.verifyNoteOwner(noteId, userId);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        throw error;
+      }
+
+      try {
+        await this._collaborationService.verifyCollaborator(noteId, userId);
+      } catch {
+        throw error;
+      }
     }
   }
 }
