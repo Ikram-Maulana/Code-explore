4ce0bc29f882f0c85369199664361a7c4809243e
ikram-maulana <ikram075@ummi.ac.id>
2022-06-18 19:32:28 +0700

    1.5. Menambahkan Fitur Tambah Kontak

--- Project/contacts-app/src/components/ContactApp.js
+++ Project/contacts-app/src/components/ContactApp.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ContactList from "./ContactList";
 import { getData } from "../utils/data";
+import ContactInput from "./ContactInput";
 
 class ContactApp extends React.Component {
   constructor(props) {
@@ -10,6 +11,7 @@ class ContactApp extends React.Component {
     };
 
     this.onDeleteButtonHandler = this.onDeleteButtonHandler.bind(this);
+    this.onAddContactHandler = this.onAddContactHandler.bind(this);
   }
 
   onDeleteButtonHandler(id) {
@@ -19,9 +21,28 @@ class ContactApp extends React.Component {
     this.setState({ contacts: newContacts });
   }
 
+  onAddContactHandler({ name, tag }) {
+    this.setState((prevState) => {
+      return {
+        contacts: [
+          ...prevState.contacts,
+          {
+            id: +new Date(),
+            name,
+            tag,
+            imageUrl: "/images/default.jpg",
+          },
+        ],
+      };
+    });
+  }
+
   render() {
     return (
       <div className="contact-app">
+        <h1>Aplikasi Kontak</h1>
+        <h2>Tambah Kontak</h2>
+        <ContactInput addContact={this.onAddContactHandler} />
         <h1>Daftar Kontak</h1>
         <ContactList
           contacts={this.state.contacts}
