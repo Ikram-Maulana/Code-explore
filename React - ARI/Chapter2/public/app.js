35c449d783e37e6c40355699eb408dd82d4908b1
ikram-maulana <ikram075@ummi.ac.id>
2022-06-29 14:44:56 +0700

    2.3. Chapter 2 - External Stylesheet

--- React - ARI/Chapter2/public/app.js
+++ React - ARI/Chapter2/public/app.js
@@ -1,9 +1,5 @@
 const element = /*#__PURE__*/React.createElement("div", {
-  style: {
-    width: 200,
-    height: 200,
-    backgroundColor: 'blue'
-  }
+  className: "kotak"
 });
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(element);
--- React - ARI/Chapter2/src/app.js
+++ React - ARI/Chapter2/src/app.js
@@ -1,11 +1,4 @@
-const element = (
-  <div style={{
-    width: 200,
-    height: 200,
-    backgroundColor: 'blue',
-  }}>
-  </div>
-);
+const element = <div className="kotak"></div>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(element);
