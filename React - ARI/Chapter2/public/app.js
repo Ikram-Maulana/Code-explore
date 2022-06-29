6a5d33cc70d8482516634dd7dd69ac6a854c43c5
ikram-maulana <ikram075@ummi.ac.id>
2022-06-29 15:02:21 +0700

    2.4. Chapter 2 - Event Handler

--- React - ARI/Chapter2/public/app.js
+++ React - ARI/Chapter2/public/app.js
@@ -1,5 +1,9 @@
-const element = /*#__PURE__*/React.createElement("div", {
-  className: "kotak"
-});
+const clickEventHandler = msg => {
+  alert(msg);
+};
+
+const element = /*#__PURE__*/React.createElement("button", {
+  onClick: () => clickEventHandler("Hello World")
+}, "Click Me");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(element);
--- React - ARI/Chapter2/src/app.js
+++ React - ARI/Chapter2/src/app.js
@@ -1,4 +1,10 @@
-const element = <div className="kotak"></div>;
+const clickEventHandler = (msg) => {
+  alert(msg);
+};
+
+const element = (
+  <button onClick={() => clickEventHandler("Hello World")}>Click Me</button>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(element);
