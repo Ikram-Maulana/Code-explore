44bb7ba0d545ce92c977b7796d71e3298b1dcde7
ikram-maulana <ikram075@ummi.ac.id>
2022-06-29 15:40:12 +0700

    2.5. Chapter 2 - Component and State

--- React - ARI/Chapter2/public/app.js
+++ React - ARI/Chapter2/public/app.js
@@ -1,9 +1,52 @@
-const clickEventHandler = msg => {
-  alert(msg);
+const MinusBtn = ({
+  onClickMinus
+}) => {
+  return /*#__PURE__*/React.createElement("button", {
+    onClick: () => onClickMinus()
+  }, "-");
 };
 
-const element = /*#__PURE__*/React.createElement("button", {
-  onClick: () => clickEventHandler("Hello World")
-}, "Click Me");
+const ContentCount = ({
+  value
+}) => {
+  return /*#__PURE__*/React.createElement("span", {
+    style: {
+      margin: "0 1rem"
+    }
+  }, value);
+};
+
+const PlusBtn = ({
+  onClickPlus
+}) => {
+  return /*#__PURE__*/React.createElement("button", {
+    onClick: () => onClickPlus()
+  }, "+");
+};
+
+const App = () => {
+  const [count, setCount] = React.useState(0);
+
+  const onClickMinusHandler = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+
+    return 0;
+  };
+
+  const onClickPlusHandler = () => {
+    setCount(count + 1);
+  };
+
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(MinusBtn, {
+    onClickMinus: onClickMinusHandler
+  }), /*#__PURE__*/React.createElement(ContentCount, {
+    value: count
+  }), /*#__PURE__*/React.createElement(PlusBtn, {
+    onClickPlus: onClickPlusHandler
+  }));
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
+root.render( /*#__PURE__*/React.createElement(App, null));
--- React - ARI/Chapter2/src/app.js
+++ React - ARI/Chapter2/src/app.js
@@ -1,10 +1,45 @@
-const clickEventHandler = (msg) => {
-  alert(msg);
+const MinusBtn = ({ onClickMinus }) => {
+  return <button onClick={() => onClickMinus()}>-</button>;
 };
 
-const element = (
-  <button onClick={() => clickEventHandler("Hello World")}>Click Me</button>
-);
+const ContentCount = ({ value }) => {
+  return (
+    <span
+      style={{
+        margin: "0 1rem",
+      }}
+    >
+      {value}
+    </span>
+  );
+};
+
+const PlusBtn = ({ onClickPlus }) => {
+  return <button onClick={() => onClickPlus()}>+</button>;
+};
+
+const App = () => {
+  const [count, setCount] = React.useState(0);
+
+  const onClickMinusHandler = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+    return 0;
+  };
+
+  const onClickPlusHandler = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <>
+      <MinusBtn onClickMinus={onClickMinusHandler} />
+      <ContentCount value={count} />
+      <PlusBtn onClickPlus={onClickPlusHandler} />
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
+root.render(<App />);
