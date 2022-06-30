cfd84d8633244e29ff6244623212fde722053580
ikram-maulana <ikram075@ummi.ac.id>
2022-06-30 19:13:46 +0700

    2.14. Chapter 2 - Sinkronasi Data Fetching Dengan UI

--- React - ARI/Chapter2/fetch-data/public/app.js
+++ React - ARI/Chapter2/fetch-data/public/app.js
@@ -1,9 +1,27 @@
+const Loading = () => {
+  return /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("i", null, "Loading Data..."));
+};
+
+const Body = ({
+  newsData
+}) => {
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("ul", null, newsData.map(item => {
+    return /*#__PURE__*/React.createElement("li", {
+      key: item.id
+    }, item.title);
+  })));
+};
+
 const App = () => {
+  const [news, setNews] = React.useState([]);
+  const [loading, setLoading] = React.useState(true); // Side-effects
+
   React.useEffect(() => {
     const getData = async () => {
       try {
         const response = await axios.get("https://api.spaceflightnewsapi.net/v3/blogs");
-        console.log(response.data);
+        setNews(response.data);
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
@@ -11,7 +29,9 @@ const App = () => {
 
     getData();
   }, []);
-  return /*#__PURE__*/React.createElement("h1", null, "Fetch Data");
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Fetch Data"), loading ? /*#__PURE__*/React.createElement(Loading, null) : /*#__PURE__*/React.createElement(Body, {
+    newsData: news
+  }));
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
