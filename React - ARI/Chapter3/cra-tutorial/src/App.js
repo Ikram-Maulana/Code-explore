c4c660be488a32196b8c70fe5034ddb029869ae7
ikram-maulana <ikram075@ummi.ac.id>
2022-07-05 17:24:00 +0700

    3.6. Chapter 3 - Memahami Static File (Assets)

--- React - ARI/Chapter3/cra-tutorial/src/App.js
+++ React - ARI/Chapter3/cra-tutorial/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ButtonApp from "./ButtonApp";
 
+// Styling
+import "./app.css";
+
 function App() {
   return (
     <div className="App">
+      <img src="/logo192.png" alt="" />
       <ButtonApp>Click Me</ButtonApp>
     </div>
   );
