87fa07d9b8d007a37acbfa81a1dec90df124fb4a
ikram-maulana <ikram075@ummi.ac.id>
2022-07-11 11:25:04 +0700

    08: 📙 Memahami Konsep Reducer Studi Kasus 2
    Studi kasus penambahan (+2) atau pengurangan (-2) pada state

--- ReactContext - ARI/react-context-tutorial/src/components/Reducer.js
+++ ReactContext - ARI/react-context-tutorial/src/components/Reducer.js
@@ -3,9 +3,9 @@ import React, { useReducer } from "react";
 const reducer = (state, action) => {
   switch (action.type) {
     case "increment":
-      return state + 1;
+      return state + action.payload;
     case "decrement":
-      return state === 0 ? 0 : state - 1;
+      return state === 0 ? 0 : state - action.payload;
     default:
       throw new Error("Unexpected action");
   }
@@ -23,9 +23,13 @@ const Reducer = () => {
           gap: "1rem",
         }}
       >
-        <button onClick={() => setCount({ type: "decrement" })}>-</button>
+        <button onClick={() => setCount({ type: "decrement", payload: 2 })}>
+          - 2
+        </button>
         <span>{count}</span>
-        <button onClick={() => setCount({ type: "increment" })}>+</button>
+        <button onClick={() => setCount({ type: "increment", payload: 2 })}>
+          + 2
+        </button>
       </div>
     </>
   );
