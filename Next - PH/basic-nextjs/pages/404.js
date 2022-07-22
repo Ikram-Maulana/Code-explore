41671e4b0a128b52bc344cc6f72afaad840c3f32
ikram-maulana <ikram075@ummi.ac.id>
2022-07-22 07:29:30 +0700

    08: 🌌 Custom halaman 404 (Not Found Page)

---
+++ Next - PH/basic-nextjs/pages/404.js
@@ -0,0 +1,10 @@
+const CustomNotFound = () => {
+  return (
+    <div className="container-not-found">
+      <h1 className="title-not-found">Ooops...</h1>
+      <p className="title-not-found">Page not found</p>
+    </div>
+  );
+};
+
+export default CustomNotFound;
